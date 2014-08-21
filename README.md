fe5f0fae50b3acf8054ba56762c9dacca445806c
Aaron Xiao <xiaoxiangquan@gmail.com>
2014-08-21 23:18:39 +0800

    ADD: js that parses page and requests webfonts subsets

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ webfonts-subsetting
 Tailor / Subsetting font files for web page automatically. The tailored font will be used for webfonts service, and only contains th
 
 It's useful for those languages with massive glyphs, such as Chinese Hanzi.
+
+== Dependencies ==
+
+jQuery
+jQeury.json
---
+++ src/webfonts.subsetting.js
@@ -0,0 +1,126 @@
+( function ( $ ) {
+
+	/**
+	 * Configs: YOU SHOULD CUSTOMIZE HERE
+	 */
+	// fonts' names that need subsetting
+	var TARGET_FONTS = {
+		'WenQuanYi Micro Hei' : 1,
+		'UnifrakturMaguntia' : 1
+	};
+
+	/**
+	 * Constants
+	 */
+	var ELEMENT_TYPE = 1,
+		TEXT_TYPE = 3;
+
+	/**
+	 * Globals
+	 */
+	// { fontName: charsToBeSubsetted, ... }
+	var gSubsets = {};
+
+	/*
+	 * Traverse the DOM tree and process all text nodes
+	 */
+	function traverseTextNode( element, processFunc ) {
+		for ( var i in element.childNodes ) {
+			var node = element.childNodes[i];
+			switch ( node.nodeType ) {
+			case ELEMENT_TYPE:
+				traverseTextNode( node, processFunc );
+				break;
+			case TEXT_TYPE:
+				processFunc( node );
+				break;
+			}
+		}
+	}
+
+	/*
+	 * Collect subsettings to the gSubsets
+	 */
+	function collectSubsets( textNode ) {
+		var parent = textNode.parentNode,
+			text = textNode.nodeValue;
+
+		var fontFamilyStr = getComputedStyle( parent, null ).getPropertyValue('font-family');
+		var fontFamilyArray = fontFamilyStr.split( ',' );
+
+		for ( var i in fontFamilyArray ) {
+			// remove leading and trailing quotes and spaces
+			var font = fontFamilyArray[i].replace( /'|"/g, "" ).replace( /(^\s*)|(\s*$)/g, '' );
+			// not the fonts we concern
+			if ( !TARGET_FONTS.hasOwnProperty( font ) ) {
+				continue;
+			}
+
+			gSubsets[font] = gSubsets[font] || {};
+			// add every char to the container
+			for ( var j in text ) {
+				gSubsets[font][ text[j] ] = 1;
+			}
+		}
+	}
+
+	/**
+	 * Generate css according to the urls returned, and apply to the page
+	 */
+	function applyWebfontsSubsetts( fontUrls ) {
+		var cssStr = '';
+
+		for ( var font in fontUrls ) {
+			cssStr += '\
+				@font-face {\
+					font-family: "' + font + '";\
+					src: url( "' + fontUrls[ font ] + '" );\
+				}' + "\n";
+		}
+
+		// append css to head
+		$( '<style>' )
+			.text( cssStr )
+			.appendTo( $( 'head' ) );
+	}
+
+	/**
+	 * Send the subsets wanted to server
+	 */
+	function requestWebfontsSubset() {
+		var subsetsStr = $.toJSON( gSubsets );
+
+		$.ajax( {
+			url : "../src/WebfontsSubsets.php",
+			type : 'POST',
+			data : subsetsStr,
+			dataType : 'json',
+
+			success : function( data, status, xhr ) {
+				// it is something like { font:url, ... }
+				var fontsUrl = $.parseJSON( data );
+				applyWebfontsSubsetts( fontsUrl );
+			},
+
+			// handle errors
+			Error : function( xhr, error, exception ) {
+				alert(exception.toString());
+			}
+	    });
+	}
+
+	$( document ).ready( function() {
+		traverseTextNode( document.body, collectSubsets );
+		for ( var font in gSubsets ) {
+			var uniqChars = '';
+			for ( var ch in gSubsets[font] ) {
+				uniqChars += ch;
+			}
+			// note that the uniq chars has been sorted
+			gSubsets[font] = uniqChars;
+		}
+
+		requestWebfontsSubset();
+	} );
+
+}( jQuery ) );
